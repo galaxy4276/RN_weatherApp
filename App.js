22f9dd9acae2955f80defff955e59b30537e4598
galaxy4276 <galaxyhi4276@gmail.com>
2020-11-12 15:26:43 +0900

    feat: Weather.js 작성

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Loading from './loading';
+import Weather from './Weather';
 import * as Location from 'expo-location';
 import { Alert, Text } from 'react-native';
 import axios from 'axios';
@@ -9,11 +10,13 @@ const API_KEY = '2ba06918e6d404a91d3c76411fb92d3c';
 export default class extends React.Component {
   state = {
     isLoading: true,
+    temp: 0,
   };
 
   getWeather = async (latitude, longitude) => {
     const { data } = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`);
     console.log(data);
+    this.setState({ isLoading: false, temp: data.main.temp });
   }
 
   getLocation = async () => {
@@ -21,9 +24,7 @@ export default class extends React.Component {
       await Location.requestPermissionsAsync();
       const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync();
       this.getWeather(latitude, longitude);
-      this.setState({ isLoading: false });
       console.log(latitude, longitude);
-      Alert.alert("성공!", "위치 정보를 성공적으로 불러왔습니다!");   
     } catch (err) {
       Alert.alert('위치 정보를 불러올 수 없습니다.', '옵션에서 권한을 허용해주세요.');
     }
@@ -34,7 +35,7 @@ export default class extends React.Component {
   }
 
   render() {
-    const { isLoading } = this.state;
-    return isLoading ? <Loading /> : <Text style={{ paddingVertical: 50, fontSize: 30 }}>빈 화면</Text>;
-  }
+    const { isLoading, temp } = this.state;
+    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)}/>;
+  } 
 }   
